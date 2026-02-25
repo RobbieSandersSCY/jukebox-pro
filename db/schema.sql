f821fe0a2be6de56bea4c3a74616693b498905b5
Robbie Sanders <sande290@gmail.com>
2026-02-25 17:55:03 -0500

    add user table to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,12 @@ DROP TABLE IF EXISTS playlists_tracks;
 DROP TABLE IF EXISTS playlists;
 DROP TABLE IF EXISTS tracks;
 
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text NOT NULL,
+  password text NOT NULL
+)
+
 CREATE TABLE tracks (
   id serial PRIMARY KEY,
   name text NOT NULL,
@@ -11,7 +17,8 @@ CREATE TABLE tracks (
 CREATE TABLE playlists (
   id serial PRIMARY KEY,
   name text NOT NULL,
-  description text NOT NULL
+  description text NOT NULL,
+  owner_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE playlists_tracks (
